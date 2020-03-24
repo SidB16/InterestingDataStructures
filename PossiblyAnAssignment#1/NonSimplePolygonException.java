ea790784201335da8aed66f12598e93fb2fe51ef
Siddharth Bhardwaj <25156849+SidB16@users.noreply.github.com>
2020-03-24 18:50:40 -0400

    Update NonSimplePolygonException.java

--- PossiblyAnAssignment#1/NonSimplePolygonException.java
+++ PossiblyAnAssignment#1/NonSimplePolygonException.java
@@ -1,10 +1,17 @@
 package A1;
 
+/**********************************************************
+ * EECS2011: Fundamentals of Data Structures,  Winter 2019
+ * Assignment 1: Polygon Hierarchy
+ * Section:  Z 
+ * Student Name:  Siddharth Bhardwaj 
+ * Student eecs account:  sid16
+ * Student ID number:  
+ **********************************************************/
+
 /**
  * This exception is thrown whenever a method is invoked on a non-simple polygon
  * when a simple polygon is expected.
- * 
- * @author Andy Mirzaian
  */
 public class NonSimplePolygonException extends Exception {
 
